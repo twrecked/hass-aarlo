f2c3a55e14b952c4d4d4699ff1bd2ecc77a3ecce
Steve Herrell <steve.herrell@gmail.com>
2019-03-03 22:47:33 -0500

    added door to camera glance

--- www/aarlo-glance.js
+++ www/aarlo-glance.js
@@ -27,42 +27,32 @@ class AarloGlance extends LitElement {
 			}
 			.box {
 				white-space: var(--paper-font-common-nowrap_-_white-space); overflow: var(--paper-font-common-nowrap_-_overflow); text-overflow: var(--paper-font-common-nowrap_-_text-overflow);
-					position: absolute;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					background-color: rgba(0, 0, 0, 0.4);
-					padding: 4px 8px;
-					font-size: 16px;
-					line-height: 40px;
-					color: white;
-					display: flex;
-					justify-content: space-between;
+				position: absolute;
+				left: 0;
+				right: 0;
+				background-color: rgba(0, 0, 0, 0.4);
+				padding: 4px 8px;
+				font-size: 16px;
+				line-height: 36px;
+				color: white;
+				display: flex;
+				justify-content: space-between;
 			}
-			.box-small {
-				white-space: var(--paper-font-common-nowrap_-_white-space); overflow: var(--paper-font-common-nowrap_-_overflow); text-overflow: var(--paper-font-common-nowrap_-_text-overflow);
-					position: absolute;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					background-color: rgba(0, 0, 0, 0.4);
-					padding: 4px 8px;
-					font-size: 16px;
-					line-height: 30px;
-					color: white;
-					display: flex;
-					justify-content: space-between;
+			.box-top {
+				top: 0;
 			}
-			.middle {
-				left: 10%;
-				right: 10%;
-				bottom: 35%;
+			.box-bottom {
+				bottom: 0;
 			}
-			.box .title {
+			.box-bottom-small {
+				bottom: 0;
+				line-height: 30px;
+			}
+			.box-title {
 				font-weight: 500;
 				margin-left: 4px;
 			}
-			.box .status {
+			.box-status {
 				font-weight: 500;
 				margin-right: 4px;
 				text-transform: capitalize;
@@ -166,6 +156,8 @@ class AarloGlance extends LitElement {
 		var libraryPrevHidden = 'hidden';
 		var libraryNextHidden = 'hidden';
 		var imageHidden       = 'hidden';
+		var topHidden         = 'hidden';
+		var doorStatusHidden  = 'hidden';
 		var brokeHidden       = 'hidden';
 		if( _video ) {
 			var videoHidden   = '';
@@ -187,6 +179,7 @@ class AarloGlance extends LitElement {
 		} else {
 			var imageHidden   = _img != null ? '':'hidden';
 			var brokeHidden   = _img == null ? '':'hidden';
+			var topHidden     = this._topTitle || this._topStatus ? imageHidden:'hidden';
 
 			// for later use!
 			this._clientWidth  = this.clientWidth
@@ -201,6 +194,10 @@ class AarloGlance extends LitElement {
 		var soundHidden    = show.includes('sound') ? '' : 'hidden';
 		var capturedHidden = show.includes('captured') || show.includes('captured_today') ? '' : 'hidden';
 		var snapshotHidden = show.includes('snapshot') ? '' : 'hidden';
+
+		var doorHidden     = this._doorId == undefined ? 'hidden':''
+		var doorLockHidden = this._doorLockId == undefined ? 'hidden':''
+		var doorBellHidden = this._doorBellId == undefined ? 'hidden':''
 
 		if( batteryHidden == '' ) {
 			var battery      = this.safe_state(_hass,this._batteryId,0);
@@ -263,6 +260,39 @@ class AarloGlance extends LitElement {
 			var snapshotIcon  = 'mdi:camera'
 		}
 
+		if( doorHidden == '' ) {
+			var doorStatusHidden = '';
+		    var doorOn       = this.safe_state(_hass,this._doorId,'off').state == 'on' ? 'state-on' : '';
+			var doorText     = 'Door: ' + (doorOn == '' ? 'closed' : 'open');
+			var doorIcon     = doorOn == '' ? 'mdi:door' : 'mdi:door-open';
+		} else {
+			var doorOn    = 'not-used'
+			var doorText  = 'not-used'
+			var doorIcon  = 'not-used'
+		}
+
+		if( doorLockHidden == '' ) {
+			var doorStatusHidden = '';
+		    var doorLockOn       = this.safe_state(_hass,this._doorLockId,'locked').state == 'locked' ? 'state-on' : 'state-warn';
+			var doorLockText     = 'Lock: ' + (doorLockOn == 'state-on' ? 'locked (click to unlock)' : 'unlocked (click to lock)');
+			var doorLockIcon     = doorLockOn == 'state-on' ? 'mdi:lock' : 'mdi:lock-open';
+		} else {
+			var doorLockOn    = 'not-used'
+			var doorLockText  = 'not-used'
+			var doorLockIcon  = 'not-used'
+		}
+
+		if( doorBellHidden == '' ) {
+			var doorStatusHidden = '';
+		    var doorBellOn       = this.safe_state(_hass,this._doorBellId,'off').state == 'on' ? 'state-on' : '';
+			var doorBellText     = 'Bell: ' + (doorBellOn == 'state-on' ? 'ding ding!' : 'idle');
+			var doorBellIcon     = 'mdi:doorbell-video';
+		} else {
+			var doorBellOn    = 'not-used'
+			var doorBellText  = 'not-used'
+			var doorBellIcon  = 'not-used'
+		}
+
 		var img = html`
 			${AarloGlance.innerStyleTemplate}
 			<div id="aarlo-wrapper">
@@ -314,9 +344,17 @@ class AarloGlance extends LitElement {
 		`;
 
 		var state = html`
-			<div class$="box ${imageHidden}">
-				<div class="title">
-				${cameraName} 
+			<div class$="box box-top ${topHidden}">
+				<div class$="box-title ${this._topTitle?'':'hidden'}">
+					${cameraName} 
+				</div>
+				<div class$="box-status ${this._topStatus?'':'hidden'}">
+					${camera.state}
+				</div>
+			</div>
+			<div class$="box box-bottom ${imageHidden}">
+				<div class$="box-title ${this._topTitle?'hidden':''}">
+					${cameraName} 
 				</div>
 				<div>
 					<ha-icon on-click="${(e) => { this.moreInfo(this._motionId); }}" class$="${motionOn} ${motionHidden}" icon="mdi:run-fast" title="${motionText}"></ha-icon>
@@ -326,11 +364,16 @@ class AarloGlance extends LitElement {
 					<ha-icon on-click="${(e) => { this.moreInfo(this._batteryId); }}" class$="${batteryState} ${batteryHidden}" icon="mdi:${batteryIcon}" title="${batteryText}"></ha-icon>
 					<ha-icon on-click="${(e) => { this.moreInfo(this._signalId); }}" class$="state-update ${signalHidden}" icon="${signalIcon}" title="${signal_text}"></ha-icon>
 				</div>
-				<div class="status">
+				<div class$="box-status ${doorStatusHidden}">
+					<ha-icon on-click="${(e) => { this.moreInfo(this._doorId); }}" class$="${doorOn} ${doorHidden}" icon="${doorIcon}" title="${doorText}"></ha-icon>
+					<ha-icon on-click="${(e) => { this.moreInfo(this._doorBellId); }}" class$="${doorBellOn} ${doorBellHidden}" icon="${doorBellIcon}" title="${doorBellText}"></ha-icon>
+					<ha-icon on-click="${(e) => { this.toggleLock(this._doorLockId); }}" class$="${doorLockOn} ${doorLockHidden}" icon="${doorLockIcon}" title="${doorLockText}"></ha-icon>
+				</div>
+				<div class$="box-status ${this._topStatus?'hidden':''}">
 					${camera.state}
 				</div>
 			</div>
-			<div class$="box-small ${libraryHidden}">
+			<div class$="box box-bottom-small ${libraryHidden}">
 				<div >
 					<ha-icon on-click="${(e) => { this.setLibraryBase(this._library_base - 9); }}" class$="${libraryPrevHidden} state-on" icon="mdi:chevron-left" title="previous"></ha-icon>
 				</div>
@@ -374,6 +417,7 @@ class AarloGlance extends LitElement {
             throw new Error( 'missing show components' );
         }
 
+		// camera definition
         this._config = config;
 		this._cameraId  = 'camera.aarlo_' + camera;
 		this._motionId  = 'binary_sensor.aarlo_motion_' + camera;
@@ -382,10 +426,27 @@ class AarloGlance extends LitElement {
 		this._signalId  = 'sensor.aarlo_signal_strength_' + camera;
 		this._captureId = 'sensor.aarlo_captured_today_' + camera;
 		this._lastId    = 'sensor.aarlo_last_' + camera;
-
 		if ( this._hass && this._hass.states[this._cameraId] == undefined ) {
 			throw new Error( 'unknown camera' );
 		}
+
+		// door definition
+		this._doorId     = config.door ? config.door: undefined
+		this._doorBellId = config.door_bell ? config.door_bell : undefined
+		this._doorLockId = config.door_lock ? config.door_lock : undefined
+		if ( this._hass && this._door && this._hass.states[this._door] == undefined ) {
+			throw new Error( 'unknown door' )
+		}
+		if ( this._hass && this._doorBellId && this._hass.states[this._doorBellId] == undefined ) {
+			throw new Error( 'unknown door bell' )
+		}
+		if ( this._hass && this._doorLockId && this._hass.states[this._doorLockId] == undefined ) {
+			throw new Error( 'unknown door lock' )
+		}
+
+		// ui configuration
+		this._topTitle = config.top_title ? config.top_title : false
+		this._topStatus = config.top_status ? config.top_status : false
 
 		this._updateCameraImageSrc()
     }
@@ -400,6 +461,7 @@ class AarloGlance extends LitElement {
 		this.shadowRoot.dispatchEvent(event);
 		return event;
 	}
+
 
 	async readLibrary( id,at_most ) {
 		try {
@@ -468,6 +530,14 @@ class AarloGlance extends LitElement {
 			this._img = `data:${contentType};base64, ${content}`;
 		} catch (err) {
 			this._img = null
+		}
+	}
+
+	toggleLock( id ) {
+		if ( this.safe_state(this._hass,id,'locked').state == 'locked' ) {
+			this._hass.callService( 'lock','unlock', { entity_id:id } )
+		} else {
+			this._hass.callService( 'lock','lock', { entity_id:id } )
 		}
 	}
 
