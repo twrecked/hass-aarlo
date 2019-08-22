267a9b38c9bd71514d39fe9de902878a5aad5f14
Steve Herrell <steve.herrell@gmail.com>
2019-08-21 22:20:35 -0400

    bumped revision

--- custom_components/aarlo/pyaarlo/__init__.py
+++ custom_components/aarlo/pyaarlo/__init__.py
@@ -26,7 +26,7 @@ from custom_components.aarlo.pyaarlo.constant import ( BLANK_IMAGE,
 
 _LOGGER = logging.getLogger('pyaarlo')
 
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 
 class PyArlo(object):
 
