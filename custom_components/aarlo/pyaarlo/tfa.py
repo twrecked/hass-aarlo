a8ca1bbfd955bb4496d7b8b21ca66ec3ace8f408
Steve Herrell <steve.herrell@gmail.com>
2020-03-30 16:59:23 -0400

    custom 2fa support, stop on login failure

--- custom_components/aarlo/pyaarlo/tfa.py
+++ custom_components/aarlo/pyaarlo/tfa.py
@@ -3,12 +3,33 @@ import imaplib
 import re
 import time
 
-from .constant import (
-    TFA_CONSOLE_SOURCE,
-    TFA_IMAP_SOURCE)
+
+class Arlo2FAConsole:
+    """ 2FA authentication via console.
+    Accepts input from console and returns that for 2FA.
+    """
+
+    def __init__(self, arlo):
+        self._arlo = arlo
+
+    def start(self):
+        self._arlo.debug('2fa-console: starting')
+        return True
+
+    def get(self):
+        self._arlo.debug('2fa-console: checking')
+        return input('Enter Code: ')
+
+    def stop(self):
+        self._arlo.debug('2fa-console: stopping')
 
 
-class Arlo2FA:
+class Arlo2FAImap:
+    """ 2FA authentication via IMAP
+    Connects to IMAP server and waits for email from Arlo with 2FA code in it.
+
+    Note: will probably need tweaking for other IMAP setups...
+    """
 
     def __init__(self, arlo):
         self._arlo = arlo
@@ -17,90 +38,85 @@ class Arlo2FA:
         self._new_ids = None
 
     def start(self):
-        self._arlo.debug('2fa is using {}'.format(self._arlo.cfg.tfa_source))
+        self._arlo.debug('2fa-imap: starting')
 
-        # console, always works!
-        if self._arlo.cfg.tfa_source == TFA_CONSOLE_SOURCE:
+        # clean up
+        if self._imap is not None:
+            self.stop()
+
+        self._imap = imaplib.IMAP4_SSL(self._arlo.cfg.imap_host)
+        res, status = self._imap.login(self._arlo.cfg.imap_username, self._arlo.cfg.imap_password)
+        if res.lower() != 'ok':
+            self._arlo.debug('imap login failed')
+            return False
+        res, status = self._imap.select()
+        if res.lower() != 'ok':
+            self._arlo.debug('imap select failed')
+            return False
+        res, self._old_ids = self._imap.search(None, 'FROM', 'do_not_reply@arlo.com')
+        if res.lower() != 'ok':
+            self._arlo.debug('imap search failed')
+            return False
+
+        self._new_ids = self._old_ids
+        self._arlo.debug("old-ids={}".format(self._old_ids))
+        if res.lower() == 'ok':
             return True
-
-        # imap, grab snapshot of current IDs
-        elif self._arlo.cfg.tfa_source == TFA_IMAP_SOURCE:
-            self._imap = imaplib.IMAP4_SSL(self._arlo.cfg.imap_host)
-            res, status = self._imap.login(self._arlo.cfg.imap_username, self._arlo.cfg.imap_password)
-            if res.lower() != 'ok':
-                self._arlo.debug('imap login failed')
-                return False
-            res, status = self._imap.select()
-            if res.lower() != 'ok':
-                self._arlo.debug('imap select failed')
-                return False
-            res, self._old_ids = self._imap.search(None, 'FROM', 'do_not_reply@arlo.com')
-            if res.lower() != 'ok':
-                self._arlo.debug('imap search failed')
-                return False
-
-            self._new_ids = self._old_ids
-            self._arlo.debug("old-ids={}".format(self._old_ids))
-            if res.lower() == 'ok':
-                return True
 
         return False
 
     def get(self):
-        self._arlo.debug('2fa checking')
-
-        # console, wait until the user finishes
-        if self._arlo.cfg.tfa_source == TFA_CONSOLE_SOURCE:
-            return input('Enter Code: ')
+        self._arlo.debug('2fa-imap: checking')
 
         # give tfa_total_timeout seconds for email to arrive
-        elif self._arlo.cfg.tfa_source == TFA_IMAP_SOURCE:
+        start = time.time()
+        while True:
 
-            start = time.time()
-            while True:
+            # wait a short while, stop after a total timeout
+            # ok to do on first run gives email time to arrive
+            time.sleep(self._arlo.cfg.tfa_timeout)
+            if time.time() > (start + self._arlo.cfg.tfa_total_timeout):
+                return None
 
-                # wait a short while, stop after a total timeout
-                # ok to do on first run gives email time to arrive
-                time.sleep(self._arlo.cfg.tfa_timeout)
-                if time.time() > (start + self._arlo.cfg.tfa_total_timeout):
-                    return None
+            # grab new email ids
+            self._imap.check()
+            res, self._new_ids = self._imap.search(None, 'FROM', 'do_not_reply@arlo.com')
+            self._arlo.debug("new-ids={}".format(self._new_ids))
+            if self._new_ids == self._old_ids:
+                self._arlo.debug("no change in emails")
+                continue
 
-                # grab new email ids
-                self._imap.check()
-                res, self._new_ids = self._imap.search(None, 'FROM', 'do_not_reply@arlo.com')
-                self._arlo.debug("new-ids={}".format(self._new_ids))
-                if self._new_ids == self._old_ids:
-                    self._arlo.debug("no change in emails")
+            # new message...
+            old_ids = self._old_ids[0].split()
+            for msg_id in self._new_ids[0].split():
+
+                # seen it?
+                if msg_id in old_ids:
                     continue
 
-                # new message...
-                old_ids = self._old_ids[0].split()
-                for msg_id in self._new_ids[0].split():
+                # new message. look for HTML part and look code code in it
+                self._arlo.debug("new-msg={}".format(msg_id))
+                res, msg = self._imap.fetch(msg_id, '(RFC822)')
+                for part in email.message_from_bytes(msg[0][1]).walk():
+                    if part.get_content_type() == 'text/html':
+                        for line in part.get_payload().splitlines():
 
-                    # seen it?
-                    if msg_id in old_ids:
-                        continue
+                            # match code in email, this might need some work if the email changes
+                            code = re.match(r'^\W*(\d{6})\W*$', line)
+                            if code is not None:
+                                self._arlo.debug("code={}".format(code.group(1)))
+                                return code.group(1)
 
-                    # new message. look for HTML part and look code code in it
-                    self._arlo.debug("new-msg={}".format(msg_id))
-                    res, msg = self._imap.fetch(msg_id, '(RFC822)')
-                    for part in email.message_from_bytes(msg[0][1]).walk():
-                        if part.get_content_type() == 'text/html':
-                            for line in part.get_payload().splitlines():
-
-                                # match code in email, this might need some work if the email changes
-                                code = re.match(r'^\W*(\d{6})\W*$', line)
-                                if code is not None:
-                                    self._arlo.debug("code={}".format(code.group(1)))
-                                    return code.group(1)
-
-                # update old so we don't keep trying new
-                self._old_ids = self._new_ids
+            # update old so we don't keep trying new
+            self._old_ids = self._new_ids
 
         return None
 
     def stop(self):
+        self._arlo.debug('2fa-imap: stopping')
 
-        if self._arlo.cfg.tfa_source == TFA_IMAP_SOURCE:
-            self._imap.close()
-            self._imap.logout()
+        self._imap.close()
+        self._imap.logout()
+        self._imap = None
+        self._old_ids = None
+        self._new_ids = None
