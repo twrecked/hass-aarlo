a30921fa6791de4f28192b94a980b5c8ebfe959e
Steve Herrell <steve.herrell@gmail.com>
2020-12-07 21:13:00 -0500

    Bumped revisions.

--- custom_components/aarlo/__init__.py
+++ custom_components/aarlo/__init__.py
@@ -28,7 +28,7 @@ from requests.exceptions import ConnectTimeout, HTTPError
 
 from .pyaarlo.constant import DEFAULT_AUTH_HOST, DEFAULT_HOST, SIREN_STATE_KEY
 
-__version__ = "0.7.0"
+__version__ = "0.7.0.2"
 
 _LOGGER = logging.getLogger(__name__)
 
