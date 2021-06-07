98ca2702f40ab1432b35490f4878369617f2eae6
Steve Herrell <steve.herrell@gmail.com>
2021-06-06 22:34:08 -0400

    Bumped revision.

--- custom_components/aarlo/__init__.py
+++ custom_components/aarlo/__init__.py
@@ -27,7 +27,7 @@ from homeassistant.helpers import config_validation as cv
 from pyaarlo.constant import DEFAULT_AUTH_HOST, DEFAULT_HOST, SIREN_STATE_KEY
 from requests.exceptions import ConnectTimeout, HTTPError
 
-__version__ = "0.8.0a1"
+__version__ = "0.8.0a2"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -76,6 +76,7 @@ CONF_SAVE_UPDATES_TO = "save_updates_to"
 CONF_USER_STREAM_DELAY = "user_stream_delay"
 CONF_SAVE_MEDIA_TO = "save_media_to"
 CONF_NO_UNICODE_SQUASH = "no_unicode_squash"
+CONF_SAVE_SESSION = "save_session"
 
 SCAN_INTERVAL = timedelta(seconds=60)
 PACKET_DUMP = False
@@ -113,6 +114,7 @@ SAVE_UPDATES_TO = ""
 USER_STREAM_DELAY = 1
 SAVE_MEDIA_TO = ""
 NO_UNICODE_SQUASH = True
+SAVE_SESSION = True
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -182,6 +184,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Optional(
                     CONF_NO_UNICODE_SQUASH, default=NO_UNICODE_SQUASH
                 ): cv.boolean,
+                vol.Optional(CONF_SAVE_SESSION, default=SAVE_SESSION): cv.boolean,
             }
         ),
     },
@@ -360,6 +363,7 @@ def login(hass, conf):
     save_media_to = conf.get(CONF_SAVE_MEDIA_TO)
     user_stream_delay = conf.get(CONF_USER_STREAM_DELAY)
     no_unicode_squash = conf.get(CONF_NO_UNICODE_SQUASH)
+    save_session = conf.get(CONF_SAVE_SESSION)
 
     # Fix up config
     if conf_dir == "":
@@ -411,6 +415,7 @@ def login(hass, conf):
                 user_stream_delay=user_stream_delay,
                 no_unicode_squash=no_unicode_squash,
                 save_media_to=save_media_to,
+                save_session=save_session,
                 wait_for_initial_setup=False,
                 verbose_debug=verbose_debug,
             )
