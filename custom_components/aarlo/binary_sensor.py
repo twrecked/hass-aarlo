8188ad13f7c1fc3978fb942babb27130c667a0e2
Nick Mowen <nickmowen213@gmail.com>
2021-11-28 11:38:09 -0700

    Clean up constants in binary_sensor.py

--- custom_components/aarlo/binary_sensor.py
+++ custom_components/aarlo/binary_sensor.py
@@ -13,7 +13,7 @@ from homeassistant.const import ATTR_ATTRIBUTION, CONF_MONITORED_CONDITIONS
 from homeassistant.core import callback
 from homeassistant.helpers.config_validation import PLATFORM_SCHEMA
 
-from . import COMPONENT_ATTRIBUTION, COMPONENT_BRAND, COMPONENT_DATA, COMPONENT_DOMAIN
+from .const import COMPONENT_ATTRIBUTION, COMPONENT_BRAND, COMPONENT_DATA, COMPONENT_DOMAIN
 from .pyaarlo.constant import (
     AUDIO_DETECTED_KEY,
     BUTTON_PRESSED_KEY,
