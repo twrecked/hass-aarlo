70c786daa00585488004775bb460e5757641cc40
Steve Herrell <steve.herrell@gmail.com>
2021-12-11 10:32:28 -0500

    Update resources from refreshed device list.
    Add back removed function.

--- custom_components/aarlo/pyaarlo/device.py
+++ custom_components/aarlo/pyaarlo/device.py
@@ -77,11 +77,7 @@ class ArloDevice(object):
 
         # Find properties. Event either contains a item called properties or it
         # is the whole thing.
-        props = event.get("properties", event)
-        for key in RESOURCE_KEYS + RESOURCE_UPDATE_KEYS:
-            value = props.get(key, None)
-            if value is not None:
-                self._save_and_do_callbacks(key, value)
+        self.update_resources(event.get("properties", event))
 
     def _do_callbacks(self, attr, value):
         cbs = []
