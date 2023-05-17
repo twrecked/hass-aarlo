e2193a8b2b50441a0aa5682c9dea307d39530127
Steve Herrell <steve.herrell@gmail.com>
2023-05-17 07:06:43 -0400

    Ping wired doorbell devices always.

--- custom_components/aarlo/__init__.py
+++ custom_components/aarlo/__init__.py
@@ -33,7 +33,7 @@ from .pyaarlo.constant import (
     SIREN_STATE_KEY
 )
 
-__version__ = "0.7.4b12"
+__version__ = "0.7.4b13"
 
 _LOGGER = logging.getLogger(__name__)
 
--- custom_components/aarlo/pyaarlo/__init__.py
+++ custom_components/aarlo/pyaarlo/__init__.py
@@ -44,7 +44,7 @@ from .util import time_to_arlotime
 
 _LOGGER = logging.getLogger("pyaarlo")
 
-__version__ = "0.7.4b12"
+__version__ = "0.7.4b13"
 
 
 class PyArlo(object):
