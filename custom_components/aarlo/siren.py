413696125b7f41eb0f1943046b2114ecff0f60e5
Steve Herrell <steve.herrell@gmail.com>
2023-11-25 15:25:29 -0500

    Intermediate Siren support. The original devices are still created.

--- custom_components/aarlo/siren.py
+++ custom_components/aarlo/siren.py
@@ -42,13 +42,11 @@ async def async_setup_entry(
 ) -> None:
     """Set up an Arlo IP Siren."""
 
-    _LOGGER.debug("here")
-
     arlo = hass.data.get(COMPONENT_DATA)
     if not arlo:
         return
 
-    # See what cameras and bases have sirens.
+    # See what devices have sirens.
     all_devices = []
     for base in arlo.base_stations:
         if base.has_capability(SIREN_STATE_KEY):
@@ -60,6 +58,7 @@ async def async_setup_entry(
         if doorbell.has_capability(SIREN_STATE_KEY):
             all_devices.append(doorbell)
 
+    # We have at least one.
     if all_devices:
         sirens = []
         for device in all_devices:
@@ -70,10 +69,10 @@ async def async_setup_entry(
         async_add_entities(sirens)
 
 
-class AarloSiren2(SirenEntity):
+class AarloSirenBase(SirenEntity):
     """Representation of an Aarlo Siren."""
 
-    def __init__(self, sirens, name, device_id, unique_id, ):
+    def __init__(self, sirens, name, unique_id, device_id, ):
 
         self._sirens = sirens
 
@@ -93,7 +92,7 @@ class AarloSiren2(SirenEntity):
         )
 
         self.entity_id = f"{SIREN_DOMAIN}.{COMPONENT_DOMAIN}_{slugify(self._attr_name)}"
-        _LOGGER.info(f"ArloSiren: {self._attr_name} created")
+        _LOGGER.info(f"ArloSirenBase: {self._attr_name} created")
 
     async def async_added_to_hass(self):
         """Register callbacks."""
@@ -101,7 +100,7 @@ class AarloSiren2(SirenEntity):
         @callback
         def update_state(_device, attr, value):
             _LOGGER.debug(f"siren-callback:{self._attr_name}:{attr}:{str(value)[:80]}")
-            self._attr_is_on = to_bool(value)
+            self._attr_is_on = any(to_bool(siren.siren_state) for siren in self._sirens)
             self.async_schedule_update_ha_state()
 
         for siren in self._sirens:
@@ -110,102 +109,48 @@ class AarloSiren2(SirenEntity):
 
     def turn_on(self, **kwargs):
         """Turn the sirens on."""
-        _LOGGER.debug(f"args={kwargs}")
         volume = int(kwargs.get("volume_level", 1.0) * 8)
         duration = kwargs.get("duration", 10)
         for siren in self._sirens:
             _LOGGER.debug(f"{self._attr_name} {siren.name} vol={volume}, dur={duration}")
+            siren.siren_on(duration=duration, volume=volume)
+
+        # Flip us on. update_state should confirm this.
+        self._attr_is_on = True
+        self.async_schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         """Turn the sirens off."""
-        _LOGGER.debug(f"args={kwargs}")
         for siren in self._sirens:
             _LOGGER.debug(f"{self._attr_name} {siren.name} off")
+            siren.siren_off()
 
 
-class AarloSiren(SirenEntity):
+class AarloSiren(AarloSirenBase):
     """Representation of an Aarlo Siren."""
 
     def __init__(self, device):
 
-        self._siren = device
-
-        self._attr_name = f"{device.name} Siren"
-        self._attr_unique_id = f"{COMPONENT_DOMAIN}_siren_{device.device_id}"
-        self._attr_is_on = False
-        self._attr_should_poll = False
-        self._attr_supported_features = SirenEntityFeature(
-            SirenEntityFeature.DURATION |
-            SirenEntityFeature.TURN_ON |
-            SirenEntityFeature.TURN_OFF |
-            SirenEntityFeature.VOLUME_SET
-        )
-        self._attr_device_info = DeviceInfo(
-            identifiers={(COMPONENT_DOMAIN, self._siren.device_id)},
-            manufacturer=COMPONENT_BRAND,
+        super().__init__(
+            [device],
+            f"{device.name} Siren",
+            f"{COMPONENT_DOMAIN}_siren_{device.device_id}",
+            device.device_id
         )
 
-        self.entity_id = f"{SIREN_DOMAIN}.{COMPONENT_DOMAIN}_{slugify(self._attr_name)}"
         _LOGGER.info(f"ArloSiren: {self._attr_name} created")
 
-    async def async_added_to_hass(self):
-        """Register callbacks."""
 
-        @callback
-        def update_state(_device, attr, value):
-            _LOGGER.debug(f"siren-callback:{self._attr_name}:{attr}:{str(value)[:80]}")
-            self._attr_is_on = to_bool(value)
-            self.async_schedule_update_ha_state()
-
-        _LOGGER.debug(f"register siren callbacks for {self._siren.name}")
-        self._siren.add_attr_callback(SIREN_STATE_KEY, update_state)
-
-    def turn_on(self, **kwargs):
-        """Turn the switch on."""
-        _LOGGER.debug(f"args={kwargs}")
-        volume = int(kwargs.get("volume_level", 1.0) * 8)
-        duration = kwargs.get("duration", 10)
-        _LOGGER.debug(f"{self._attr_name} vol={volume}, dur={duration}")
-
-    def turn_off(self, **kwargs):
-        """Turn the switch off."""
-        _LOGGER.debug(f"args={kwargs}")
-        _LOGGER.debug(f"{self._attr_name} off")
-
-
-class AarloAllSirens(SirenEntity):
+class AarloAllSirens(AarloSirenBase):
     """Representation of an Aarlo Siren."""
 
     def __init__(self, arlo, devices):
 
-        self._sirens = devices
-
-        self._attr_name = f"All Arlo Sirens"
-        self._attr_unique_id = f"{COMPONENT_DOMAIN}_all_sirens"
-        self._attr_is_on = False
-        self._attr_should_poll = False
-        self._attr_supported_features = SirenEntityFeature(
-            SirenEntityFeature.DURATION |
-            SirenEntityFeature.TURN_ON |
-            SirenEntityFeature.TURN_OFF |
-            SirenEntityFeature.VOLUME_SET
-        )
-        self._attr_device_info = DeviceInfo(
-            identifiers={(COMPONENT_DOMAIN, arlo.device_id)},
-            manufacturer=COMPONENT_BRAND,
+        super().__init__(
+            devices,
+            "All Arlo Sirens",
+            f"{COMPONENT_DOMAIN}_all_sirens",
+            arlo.device_id
         )
 
-        self.entity_id = f"{SIREN_DOMAIN}.{COMPONENT_DOMAIN}_{slugify(self._attr_name)}"
-        _LOGGER.info(f"ArloAllSirens: created")
-
-    def turn_on(self, **kwargs):
-        """Turn the switch on."""
-        _LOGGER.debug(f"args={kwargs}")
-        volume = int(kwargs.get("volume_level", 1.0) * 8)
-        duration = kwargs.get("duration", 10)
-        _LOGGER.debug(f"{self._attr_name} vol={volume}, dur={duration}")
-
-    def turn_off(self, **kwargs):
-        """Turn the switch off."""
-        _LOGGER.debug(f"args={kwargs}")
-        _LOGGER.debug(f"{self._attr_name} off")
+        _LOGGER.info(f"ArloAllSirens: {self._attr_name} created")
