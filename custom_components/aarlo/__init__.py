6292c2da013938a8f3d7ceeb359b21b767c1e6eb
Steve Herrell <steve.herrell@gmail.com>
2024-03-13 07:27:51 -0400

    Bump pyaarlo revision...

--- custom_components/aarlo/__init__.py
+++ custom_components/aarlo/__init__.py
@@ -53,7 +53,7 @@ from .utils import get_entity_from_domain
 from .cfg import BlendedCfg, PyaarloCfg
 
 
-__version__ = "0.8.1a11"
+__version__ = "0.8.1a12"
 
 _LOGGER = logging.getLogger(__name__)
 
