5de13b9f5420051db7e587c0b08fab78c5bc7d63
Steve Herrell <steve.herrell@gmail.com>
2024-07-03 23:03:58 -0400

    Remove an event io error.

--- custom_components/aarlo/config_flow.py
+++ custom_components/aarlo/config_flow.py
@@ -161,7 +161,7 @@ class AarloFlowHandler(config_entries.ConfigFlow, domain=COMPONENT_DOMAIN):
         """Import momentary config from configuration.yaml."""
 
         _LOGGER.info("importing aarlo YAML")
-        UpgradeCfg.create_file_config(self.hass, import_data)
+        await UpgradeCfg.create_file_config(self.hass, import_data)
         data = UpgradeCfg.create_flow_data(import_data)
         options = UpgradeCfg.create_flow_options(import_data)
 
