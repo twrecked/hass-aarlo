63df2aa5298aa290085e43f4e966c0a95c35338a
Steve Herrell <47094394+twrecked@users.noreply.github.com>
2025-02-15 09:00:37 -0500

    Set the pyaarlo version. (#1055)
    Bring in "trusted browser" support.

--- custom_components/aarlo/__init__.py
+++ custom_components/aarlo/__init__.py
@@ -53,7 +53,7 @@ from .utils import get_entity_from_domain
 from .cfg import BlendedCfg, PyaarloCfg
 
 
-__version__ = "0.8.1.15"
+__version__ = "0.8.1.16"
 
 _LOGGER = logging.getLogger(__name__)
 
